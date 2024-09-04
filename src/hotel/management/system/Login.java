1c836836086cbc71c6ecd52d58fe00c093c4a904
Atom M. Nava <atommunoznava@gmail.com>
2024-09-04 13:33:50 -0600

    Cambios Login

--- src/hotel/management/system/Login.java
+++ src/hotel/management/system/Login.java
@@ -3,8 +3,14 @@ package hotel.management.system;
 // Bibliotecas
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.ResultSet;
 
-public class Login extends JFrame {
+public class Login extends JFrame implements ActionListener {
+    JTextField userName;
+    JPasswordField password;
+    JButton login, cancel;
     Login() {
         getContentPane().setBackground(Color.WHITE);
         setLayout(null);
@@ -13,7 +19,7 @@ public class Login extends JFrame {
         user.setBounds(40,20,100,20);
         add(user);
 
-        JTextField userName = new JTextField();
+        userName = new JTextField();
         userName.setBounds(150,20,150,30);
         add(userName);
 
@@ -21,20 +27,22 @@ public class Login extends JFrame {
         pass.setBounds(40,70,100,20);
         add(pass);
 
-        JTextField password = new JTextField();
+        password = new JPasswordField();
         password.setBounds(150,70,150,30);
         add(password);
 
-        JButton login = new JButton("Login");
+        login = new JButton("Login");
         login.setBounds(40,150,120,30);
         login.setBackground(Color.BLACK);
         login.setForeground(Color.BLACK);
+        login.addActionListener(this);
         add(login);
 
-        JButton cancel = new JButton("Cancel");
+        cancel = new JButton("Cancel");
         cancel.setBounds(180,150,120,30);
         cancel.setBackground(Color.BLACK);
         cancel.setForeground(Color.BLACK);
+        cancel.addActionListener(this);
         add(cancel);
 
         ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons-hotel/second.jpg"));
@@ -48,7 +56,34 @@ public class Login extends JFrame {
         setVisible(true);
     }
 
-    public static void main(String[] args) {
-        new Login().setVisible(true);
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == login) {
+            String tUsername = userName.getText();
+            String tPassword = password.getText();
+            try {
+                Conn conn = new Conn();
+                String query = "select * from login where username = '" + tUsername + "' and password = '" + tPassword + "'";
+                ResultSet rs = conn.s.executeQuery(query);
+
+                if (rs.next()) {
+                    setVisible(false);
+                    new Databoard().setVisible(true);
+                } else {
+                    JOptionPane.showMessageDialog(null, "Invalid Username or Password");
+                    setVisible(false);
+                }
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+        } else if (e.getSource() == cancel) {
+            setVisible(false);
+        }
     }
+    public static void main(String[] args) {
+        new Login();
+    }
+
 }
+
+
