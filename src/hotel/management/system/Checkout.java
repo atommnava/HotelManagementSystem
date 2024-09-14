fc883c3a262a208f4dbfdf48509b39a994f8d6e1
Atom M. Nava <atommunoznava@gmail.com>
2024-09-14 15:16:19 -0600

    Nueva clase Checkout

---
+++ src/hotel/management/system/Checkout.java
@@ -0,0 +1,18 @@
+package hotel.management.system;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Checkout extends JFrame implements ActionListener {
+    public Checkout(){
+
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+    public static void main(String[] args) {
+        new Checkout();
+    }
+}
